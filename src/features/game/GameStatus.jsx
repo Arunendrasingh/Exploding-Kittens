9c928626f950cf3c05ef962101dd02cd03aacca8
Arunendra Singh <singharunendra978@gmail.com>
2024-03-03 23:32:50 +0530

    Add Style for GameStatus component.

--- src/features/game/GameStatus.jsx
+++ src/features/game/GameStatus.jsx
@@ -1,5 +1,24 @@
 function GameStatus() {
-  return <div>You Lost the Game</div>;
+  return (
+    <div className="container mt-5">
+      <div className="row align-item-center text-center">
+        <div className="row">
+          <h1>
+            😢☹️☹️☹️😢 <br />
+            Oops! 🎮 You lost the game this time. No worries, though! Every
+            setback is just a set-up for a jaw-dropping comeback. Better luck on
+            your next attempt! <br />
+            💪🍀 🌌🚀
+          </h1>
+        </div>
+
+        <div className="row justify-content-evenly mt-5">
+          <button className="col-4 btn btn-primary">Start New Game</button>
+          <button className="col-4 btn btn-danger">Quit & Logout</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GameStatus;
