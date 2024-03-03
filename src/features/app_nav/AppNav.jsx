e36e8a65d2cc9eb1f2432fe4a13efe035281a210
Arunendra Singh <singharunendra978@gmail.com>
2024-03-03 22:52:55 +0530

    Add useScore & userName in Navbar.

--- src/features/app_nav/AppNav.jsx
+++ src/features/app_nav/AppNav.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function AppNav() {
   const { totalLifeline } = useSelector((state) => state.game);
-  console.log(totalLifeline, typeof totalLifeline);
+  const { userScore, userName } = useSelector((state) => state.user);
   return (
     <nav
       className="navbar bg-primary navbar-expand-lg bg-body-primary"
@@ -32,27 +32,27 @@ function AppNav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">
                 Home
               </a>
-            </li>
+            </li> */}
             <li className="nav-item">
               <a className="nav-link" href="/">
                 New Game
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Quit
-              </a>
-            </li>
           </ul>
           <div className="d-flex text-white">
-            {/* <div className="me-2 px-2">❤❤❤❤❤</div> */}
+            {userName && (
+              <div className="me-2 px-2">
+                Hello <strong>{userName}</strong>
+              </div>
+            )}
             <div className="me-2 px-2">{"❤".repeat(totalLifeline)}</div>
             <div className="px-2">
-              <strong>Score:</strong> 50
+              <strong>Score: </strong>
+              {userScore}
             </div>
           </div>
         </div>
